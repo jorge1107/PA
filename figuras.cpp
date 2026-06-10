ec98d1733ff01827ef05918df8c78c0470344d5d
Jorge <jorgelucas1107@gmal.com>
2026-06-10 20:15:42 -0300

    Correções no roteiro

--- figuras.cpp
+++ figuras.cpp
@@ -1,6 +1,6 @@
 #include "figuras.h"
 
-// --- PUT e CUT VOXEL ---
+//  PUT e CUT VOXEL 
 PutVoxel::PutVoxel(int x, int y, int z, float r, float g, float b, float a) {
     this->x = x; this->y = y; this->z = z;
     this->r = r; this->g = g; this->b = b; this->a = a;
@@ -17,7 +17,7 @@ void CutVoxel::draw(Sculptor &t) {
     t.cutVoxel(x, y, z);
 }
 
-// --- PUT e CUT BOX ---
+//  PUT e CUT BOX 
 PutBox::PutBox(int x0, int x1, int y0, int y1, int z0, int z1, float r, float g, float b, float a) {
     this->x0 = x0; this->x1 = x1; this->y0 = y0; this->y1 = y1; this->z0 = z0; this->z1 = z1;
     this->r = r; this->g = g; this->b = b; this->a = a;
@@ -34,7 +34,7 @@ void CutBox::draw(Sculptor &t) {
     t.cutBox(x0, x1, y0, y1, z0, z1);
 }
 
-// --- PUT e CUT SPHERE ---
+//  PUT e CUT SPHERE 
 PutSphere::PutSphere(int xcenter, int ycenter, int zcenter, int radius, float r, float g, float b, float a) {
     this->xcenter = xcenter; this->ycenter = ycenter; this->zcenter = zcenter; this->radius = radius;
     this->r = r; this->g = g; this->b = b; this->a = a;
@@ -51,7 +51,7 @@ void CutSphere::draw(Sculptor &t) {
     t.cutSphere(xcenter, ycenter, zcenter, radius);
 }
 
-// --- PUT e CUT ELLIPSOID ---
+//  PUT e CUT ELLIPSOID 
 PutEllipsoid::PutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz, float r, float g, float b, float a) {
     this->xcenter = xcenter; this->ycenter = ycenter; this->zcenter = zcenter;
     this->rx = rx; this->ry = ry; this->rz = rz;
